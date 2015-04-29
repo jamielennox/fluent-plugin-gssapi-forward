99e60b9e0659ce58119d5b31cc53269522f50903
Jamie Lennox <jamielennox@redhat.com>
2015-04-29 14:13:23 +1000

    Initial commit
    Commit from bunder gem generator

---
+++ lib/fluent/plugin/gssapi/forward.rb
@@ -0,0 +1,11 @@
+require "fluent/plugin/gssapi/forward/version"
+
+module Fluent
+  module Plugin
+    module Gssapi
+      module Forward
+        # Your code goes here...
+      end
+    end
+  end
+end
